53714d4dfe5076aef8b1e2040b00e64626a392ce
carlaulloa <carla.ulloa.lp@gmail.com>
2021-10-18 23:40:08 -0500

    chore: terraform

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -29,11 +29,11 @@ provider "kubernetes" {
  // version                = "~> 1.9"
 }
 
-module "aforo255-cluster4" {
+module "aforo255-clusterccu" {
   source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = "aforo255-cluster4"
+  cluster_name    = "aforo255-clusterccu"
   cluster_version = "1.17"
-  subnets         = ["subnet-adfa07f2", "subnet-a3c23d82"]  #CHANGE # Donot choose subnet from us-east-1e
+  subnets         = ["subnet-a6616498", "subnet-7a2b8374"]  # en aws VPC > Subnet
   #subnets = data.aws_subnet_ids.subnets.ids
   vpc_id          = aws_default_vpc.default.id
   #vpc_id         = "vpc-1234556abcdef" 
@@ -48,11 +48,11 @@ worker_groups = [
 }
 
 data "aws_eks_cluster" "cluster" {
-  name = module.aforo255-cluster4.cluster_id
+  name = module.aforo255-clusterccu.cluster_id
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.aforo255-cluster4.cluster_id
+  name = module.aforo255-clusterccu.cluster_id
 }
 
 
