ce212ddb846da4dce768e014c369a215a0d73888
carlaulloa <carla.ulloa.lp@gmail.com>
2021-10-21 01:29:36 -0500

    chore: add azure tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "example" {
+data "azurerm_resource_group" "example" {
   name     = "gr01-devops"
   location = "eastus"
 }
