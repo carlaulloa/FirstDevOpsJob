355c67af817b8560f196bb82cf1d1fa9319fc028
carlaulloa <carla.ulloa.lp@gmail.com>
2021-10-21 01:06:18 -0500

    chore: add azure tf

---
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -0,0 +1,47 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "gr01-devops"
+  location = "eastus"
+}
+
+resource "azurerm_kubernetes_cluster" "example" {
+  name                = "aks-service-ccu"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  dns_prefix          = "aks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_B2s"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  addon_profile {
+    aci_connector_linux {
+      enabled = false
+    }
+
+    azure_policy {
+      enabled = false
+    }
+
+    http_application_routing {
+      enabled = false
+    }
+
+    kube_dashboard {
+      enabled = true
+    }
+
+    oms_agent {
+      enabled = false
+    }
+  }
+}
