fc3c5077af3b6c56d289ec81d6b990f550e80078
Carla Contreras Ulloa <carla.ulloa.lp@gmail.com>
2021-10-22 00:02:16 -0500

    Update main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -4,9 +4,9 @@ provider "azurerm" {
 
 terraform {
   backend  "azurerm"  {
+    resource_group_name = "gr01-devops"
     storage_account_name = "backendccu"
     container_name = "terraform"
-    access_key = "x3I1gJRGUBGG6GbBEoYg2xQLsA78Gm/BV+ifWz5oFTnyBa6Hy9rey+GpwQSJud8el7++xluYp4AClCF57l14aw=="
     key = "terraform.tfstate"
   }
 }
