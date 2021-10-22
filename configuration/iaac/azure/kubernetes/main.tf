015215e8018e34b3754c465b00b71bae8cfccf57
Carla Contreras Ulloa <carla.ulloa.lp@gmail.com>
2021-10-21 23:50:02 -0500

    Update main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -2,6 +2,10 @@ provider "azurerm" {
   features {}
 }
 
+terraform {
+  backend  "azurerm"  {}
+}
+
 resource "azurerm_resource_group" "example" {
   name     = "gr04-devops"
   location = "eastus"
